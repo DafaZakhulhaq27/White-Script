e46a31624b6918e69d2dc2e7d51279240f8057fe
Dafa Zakhulhaq <dafa27890@gmail.com>
2022-01-05 06:06:51 +0700

    create home

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+var text = [
+  `Telev<span class="blurred-text">ision</span> P<span class="blurred-text">rodu</span>c<span class="blurred-text">tion</span>`,
+  "Television Commercials",
+  "Digital Videos",
+  "Television Production",
+];
+
+var counter = 0;
+var elem = document.getElementById("bannerText");
+var sint = setInterval(change, 3000);
+
+function change() {
+  if (counter > 0) {
+    elem.classList.add("fade");
+  }
+  elem.innerHTML = text[counter];
+  counter++;
+
+  if (counter >= text.length) {
+    counter = 1;
+    // clearInterval(inst);
+  }
+}
+
+change();
