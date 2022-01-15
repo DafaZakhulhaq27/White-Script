8ec2b79051e6dd4d3ab5b9ef3f2e20e877a0f8e8
Dafa Zakhulhaq <dafa27890@gmail.com>
2022-01-15 12:29:08 +0700

    add revision

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,9 +1,8 @@
 var text = [
   `Telev<span class="blurred-text">ision</span> P<span class="blurred-text">rodu</span>c<span class="blurred-text">tion</span>`,
-  "Television Commercials",
-  "Digital Videos",
-  "Television Production",
-  "Audiobooks",
+  `Telev<span class="blurred-text">ision</span> Commerc<span class="blurred-text">ials</span>`,
+  `Digi<span class="blurred-text">tal</span> Vide<span class="blurred-text">os</span>`,
+  `Audiob<span class="blurred-text">ooks</span>`,
 ];
 
 var counter = 0;
@@ -16,11 +15,12 @@ function change() {
   // elem.classList.add("banner-text-opacity");
   elem.classList.add("fade");
   // }
+  console.log(counter, "text[counter]");
   elem.innerHTML = text[counter];
   counter++;
 
   if (counter >= text.length) {
-    counter = 1;
+    counter = 0;
     // clearInterval(inst);
   }
 }
